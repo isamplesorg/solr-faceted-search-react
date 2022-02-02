84e9cc8fb3fc038766ca79f2d970bb85b70e6a73
Matthew_GQ <qgan@email.arizona.edu>
2022-02-01 22:34:25 -0700

    make identifier clickable

--- src/components/results/result.js
+++ src/components/results/result.js
@@ -13,6 +13,11 @@ class Result extends React.Component {
 
   render() {
     const {bootstrapCss, doc, fields} = this.props;
+    const Withlink = ({field, link, children})=> (
+        field.field === "id"?
+        <a href={link}>{children}</a> :
+        children
+    );
 
     return (
       <li className={cx({"list-group-item": bootstrapCss})} onClick={() => this.props.onSelect(doc)}>
@@ -20,7 +25,9 @@ class Result extends React.Component {
           {fields.filter((field) => field.field !== "*").map((field, i) =>
             <li key={i}>
               <label>{field.label || field.field}</label>
-              {this.renderValue(field.field, doc)}
+              <Withlink field={field} link={"https://n2t.net/" + this.renderValue(field.field, doc)}>
+                {this.renderValue(field.field, doc)}
+              </Withlink>
             </li>
           )}
         </ul>
