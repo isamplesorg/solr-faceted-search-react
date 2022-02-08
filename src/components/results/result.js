1091d44c46ae8576be459d73f67ddc62f02970bd
Matthew_GQ <qgan@email.arizona.edu>
2022-02-08 15:49:22 -0700

    remove unused functions

--- src/components/results/result.js
+++ src/components/results/result.js
@@ -12,26 +12,6 @@ class Result extends React.Component {
   }
 
 
-  // Highlight the search words in the result text.
-  HighlightWords(field, text){
-    if (field.type === 'text' && field.value !== undefined){
-      // replace "&", "|", "(", ")", "*", "'", """ and duplicated whitespace with only one whitespace
-      let values = field.value.replaceAll(/\&|\*|\(|\)|\||\"|\'/g,"").replaceAll(/\s+/g," ").split(" ");
-
-      // split original text by search words insensitively by regex pattern
-      // g is for regex global and i is for insensitive.
-      values.map((value) => {
-        let regex = new RegExp(value, "gi");
-        text = text.split(regex).join("<span style='background-color:yellow;'>" + value + "</span>");
-      })
-
-    }
-
-    // How to render HTML string in React: https://medium.com/@uigalaxy7/how-to-render-html-in-react-7f3c73f5cafc
-    // dangerouslySetInnerHTML is to convert html string into react html object 
-    return <span dangerouslySetInnerHTML={{__html: text}} />;
-  }
-
   render() {
     const {bootstrapCss, doc, fields} = this.props;
 
