c30cf5b107dff264d8de26d769e8be66bd55f740
Matthew_GQ <qgan@email.arizona.edu>
2022-02-07 20:46:42 -0700

    add highlight function for search text

--- src/components/results/result.js
+++ src/components/results/result.js
@@ -11,6 +11,27 @@ class Result extends React.Component {
     return value.join(", ");
   }
 
+
+  // Highlight the search words in the result text.
+  HighlightWords(field, text){
+    if (field.type === 'text' && field.value !== undefined){
+      // replace "&", "|", "(", ")", "*", "'", """ and duplicated whitespace with only one whitespace
+      let values = field.value.replaceAll(/\&|\*|\(|\)|\||\"|\'/g,"").replaceAll(/\s+/g," ").split(" ");
+
+      // split original text by search words insensitively by regex pattern
+      // g is for regex global and i is for insensitive.
+      values.map((value) => {
+        let regex = new RegExp(value, "gi");
+        text = text.split(regex).join("<span class='bg-danger'>" + value + "</span>");
+      })
+
+    }
+
+    // How to render HTML string in React: https://medium.com/@uigalaxy7/how-to-render-html-in-react-7f3c73f5cafc
+    // dangerouslySetInnerHTML is to convert html string into react html object 
+    return <span dangerouslySetInnerHTML={{__html: text}} />;
+  }
+
   render() {
     const {bootstrapCss, doc, fields} = this.props;
 
@@ -21,9 +42,10 @@ class Result extends React.Component {
             <li key={i}>
               <label>{field.label || field.field}</label>
               {field.field === "id"?
-                <a href={"https://n2t.net/" + this.renderValue(field.field, doc)}>{this.renderValue(field.field, doc)}</a>
+                <a href={"https://n2t.net/" + this.renderValue(field.field, doc)}>{this.HighlightWords(field, this.renderValue(field.field, doc))}</a>
                 :
-                this.renderValue(field.field, doc)
+                this.HighlightWords(field, this.renderValue(field.field, doc))
+                
               }
             </li>
           )}
