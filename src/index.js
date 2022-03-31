f83063295252cbcf2e1333edcc055773efc31c40
Matthew_GQ <qgan@email.arizona.edu>
2022-03-31 13:52:13 -0700

    export solr query

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import SolrFacetedSearch from "./components/solr-faceted-search";
 import defaultComponentPack from "./components/component-pack";
 import {SolrClient} from "./api/solr-client";
+import * as solr_query from "./api/solr-query";
 
 export default SolrFacetedSearch;
 
 export {
   SolrFacetedSearch,
   defaultComponentPack,
-  SolrClient
+  SolrClient,
+  solr_query
 };
