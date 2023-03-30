77b912f3f6d493cab3ea3ca817416433caf0f352
hyunssong <hyunjusong@email.arizona.edu>
2023-03-29 20:43:16 -0700

    add single test to see if github workflow is invoked

---
+++ tests/CheckedIcon.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CheckedIcon from '../src/components/icons/checked';
+
+describe('Checked Icon', () => {
+  it('should render the checked incon', () => {
+    const { container } = render(<CheckedIcon title="test" />);
+    const svgEl = container.querySelector("[data-icon='checked']");
+    expect(svgEl.classList.toString()).toContain("checked");
+  });
+
+});
